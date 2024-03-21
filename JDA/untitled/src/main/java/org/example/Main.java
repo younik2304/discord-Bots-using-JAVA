e459d6a619a748a421b17fab9122313275aac890
YOUSSEF ANIK <youssefanik@gmail.com>
2024-03-21 21:23:56 +0000

    commiting my first jda bot

---
+++ JDA/untitled/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.sharding.DefaultShardManager;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Bot bot=new Bot();
+    }
+}
